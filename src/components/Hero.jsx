a11f3851d738e51c26675598d93a068241f220eb
santhoshaMahesh <santhoshmahesh2001@gmail.com>
2024-03-25 00:05:06 +0530

    some enhancements

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,15 +6,15 @@ const Hero = () => {
         {/* Overlay */}
         <div className='absolute w-full h-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-center'>
           <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>
-            The <span className='text-orange-500'>Best</span>
+            For all your <span className='text-blue-700'>RO purifier</span>
           </h1>
           <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>
-            <span className='text-orange-500'>Foods</span> Delivered
+            <span className='text-blue-700'>service and repair</span> needs
           </h1>
         </div>
         <img
           className='w-full max-h-[500px] object-cover'
-          src='https://fixitcasa.com/wp-content/uploads/2023/11/2.png'
+          src='./hero.webp'
           alt='/'
         />
       </div>
